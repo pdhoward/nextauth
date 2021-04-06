4bbfa1673c131b9a7125ed3f4125dcce10da83f3
pdhoward <patrick.howard@hotmail.com>
2021-04-06 11:50:25 -0500

    experiment - azure vision

---
+++ test/vision.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const {
+    ComputerVisionClient,
+    ComputerVisionModels
+  } = require("@azure/cognitiveservices-computervision")
+
+const { CognitiveServicesCredentials } = require("@azure/ms-rest-azure-js")
+
+async function main() {
+    const computerVisionKey = process.env["COMPUTERVISIONKEY"]
+    const computerVisionEndPoint =
+      process.env["COMPUTERVISIONENDPOINT"]
+    const cognitiveServiceCredentials = new CognitiveServicesCredentials(computerVisionKey);
+    const client = new ComputerVisionClient(cognitiveServiceCredentials, computerVisionEndPoint);
+  
+    const url =
+      "https://docs.microsoft.com/en-us/azure/includes/media/shared-image-galleries/shared-image-gallery.png";
+    const options = {
+      maxCandidates: 5,
+      language: "en"
+    };
+    client
+      .describeImage(url, options)
+      .then((result) => {
+        console.log("The result is:");
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log("An error occurred:");
+        console.error(err);
+      });
+  }
+  
+  main();
