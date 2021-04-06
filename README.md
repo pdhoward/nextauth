4467fb6fae11ff18bcb94f0c5346981211660084
pdhoward <patrick.howard@hotmail.com>
2021-04-06 17:37:35 -0500

    experiment - connecting to cloudinary

--- README.md
+++ README.md
@@ -20,7 +20,17 @@ Auth process using next and nodejs api server with mongo
 * docs
 https://docs.microsoft.com/en-us/azure/cognitive-services/
 
+* generate metadata for images
+https://docs.microsoft.com/en-us/azure/cognitive-services/computer-vision/tutorials/storage-lab-tutorial
+
 * custom vision
 https://docs.microsoft.com/en-us/azure/cognitive-services/Custom-Vision-Service/overview
+
+create a custom vision
+https://docs.microsoft.com/en-us/learn/modules/classify-images-with-custom-vision-service/
+
+
+api docs
+https://westcentralus.dev.cognitive.microsoft.com/docs/services/computer-vision-v3-1-ga/operations/56f91f2e778daf14a499f21b
 
 
--- test/vision.js
+++ test/vision.js
@@ -10,35 +10,83 @@ const computerVisionEndPoint =process.env["COMPUTERVISIONENDPOINT"]
 const cognitiveServiceCredentials = new CognitiveServicesCredentials(computerVisionKey);
 const client = new ComputerVisionClient(cognitiveServiceCredentials, computerVisionEndPoint);
 
+
 const items = [
-  'https://res.cloudinary.com/stratmachine/image/upload/v1617739647/objects/tablechairs_hpy9dd.jpg',
-  'https://res.cloudinary.com/stratmachine/image/upload/v1617739638/objects/packingboxes_rgonvl.jpg',
-  'https://res.cloudinary.com/stratmachine/image/upload/v1617739634/objects/packingbox_ivqebg.webp',
-  'https://res.cloudinary.com/stratmachine/image/upload/v1617739625/objects/loveseat_bm1y62.jpg',
-  'https://res.cloudinary.com/stratmachine/image/upload/v1617739621/objects/livingroomset_yxqp7u.jpg',
-  'https://res.cloudinary.com/stratmachine/image/upload/v1617739610/objects/boxqrcode_jrptw1.jpg',
-  'https://res.cloudinary.com/stratmachine/image/upload/v1589910869/qrcodes/frame_fbeose.png',
-  'https://res.cloudinary.com/stratmachine/image/upload/v1589910869/qrcodes/frame_fbeose.png',
-  'https://res.cloudinary.com/stratmachine/image/upload/v1617737720/objects/mildlee-7KKy7-TeeVs-unsplash_dqmnxt.jpg',
-  'https://res.cloudinary.com/stratmachine/image/upload/v1617737720/objects/ruchindra-gunasekara-GK8x_XCcDZg-unsplash_ymtlzu.jpg',
-  'https://res.cloudinary.com/stratmachine/image/upload/v1617737720/objects/curology-E_dRKdBhxk4-unsplash_r2y3et.jpg',
-  'https://res.cloudinary.com/stratmachine/image/upload/v1617737720/objects/mak-8wy9mGgmGoU-unsplash_ag9coq.jpg',
-  'https://res.cloudinary.com/stratmachine/image/upload/v1617737720/objects/mealpro-dd1M0Ji9xHQ-unsplash_tz0yst.jpg',
-  'https://res.cloudinary.com/stratmachine/image/upload/v1617737720/objects/brandable-box-8mCsyImZRGY-unsplash_brfz2t.jpg',
-  'https://res.cloudinary.com/stratmachine/image/upload/v1617737720/objects/magic-mind-OSk4lO--UsA-unsplash_lq3mw2.jpg',
-  'https://res.cloudinary.com/stratmachine/image/upload/v1617737720/objects/mildlee-8N6z4yXUkwY-unsplash_cqs8nk.jpg'
+  {
+  description: 'wood table and 4 chairs',
+  url:  'https://res.cloudinary.com/stratmachine/image/upload/v1617739647/objects/tablechairs_hpy9dd.jpg'
+  },
+  {
+    description: 'home depot and misc packing boxes on floor',
+    url:  'https://res.cloudinary.com/stratmachine/image/upload/v1617739638/objects/packingboxes_rgonvl.jpg'
+  },
+  {
+    description: 'A single brown packing box with lid open',
+    url:  'https://res.cloudinary.com/stratmachine/image/upload/v1617739634/objects/packingbox_ivqebg.webp'
+  },
+  {
+    description: 'Grey couch, white background',
+    url:  'https://res.cloudinary.com/stratmachine/image/upload/v1617739625/objects/loveseat_bm1y62.jpg'
+  },
+  {
+    description: 'Living room set, white coach, coffee table, fireplace',
+    url:  'https://res.cloudinary.com/stratmachine/image/upload/v1617739621/objects/livingroomset_yxqp7u.jpg'
+  },
+  {
+    description: 'White box with qrcode on face',
+    url:  'https://res.cloudinary.com/stratmachine/image/upload/v1617739610/objects/boxqrcode_jrptw1.jpg',
+  },
+  {
+    description: 'QRCode - points to proximity site',
+    url:  'https://res.cloudinary.com/stratmachine/image/upload/v1589910869/qrcodes/frame_fbeose.png'
+  },
+  {
+    description: 'Two small brown boxes - 1 leaning against the other - sitting on brown floor',
+    url:  'https://res.cloudinary.com/stratmachine/image/upload/v1617737720/objects/mildlee-7KKy7-TeeVs-unsplash_dqmnxt.jpg'
+  },
+  {
+    description: 'Warehouse aisle with shelving on both sides, stacked with items',
+    url:  'https://res.cloudinary.com/stratmachine/image/upload/v1617737720/objects/ruchindra-gunasekara-GK8x_XCcDZg-unsplash_ymtlzu.jpg'
+  },
+  {
+    description: 'Stack of small boxes - sitting in background of similar color',
+    url:  'https://res.cloudinary.com/stratmachine/image/upload/v1617737720/objects/curology-E_dRKdBhxk4-unsplash_r2y3et.jpg'
+  },
+  {
+    description: 'Open garage with stack of boxes against wall',
+    url:  'https://res.cloudinary.com/stratmachine/image/upload/v1617737720/objects/mak-8wy9mGgmGoU-unsplash_ag9coq.jpg'
+  },
+  {
+    description: 'Man holding box with mealpro printed on front',
+    url:  'https://res.cloudinary.com/stratmachine/image/upload/v1617737720/objects/mealpro-dd1M0Ji9xHQ-unsplash_tz0yst.jpg'
+  },
+  {
+    description: 'Brown box with closed lid sitting on floor',
+    url:  'https://res.cloudinary.com/stratmachine/image/upload/v1617737720/objects/brandable-box-8mCsyImZRGY-unsplash_brfz2t.jpg'
+  },
+  {
+    description: 'Brown box with grean bottom rim and closed lid sitting on floor',
+    url:  'https://res.cloudinary.com/stratmachine/image/upload/v1617737720/objects/magic-mind-OSk4lO--UsA-unsplash_lq3mw2.jpg'
+  },
+  {
+    description: 'Two brown flat boxes, not touching, sitting on brown surface',
+    url:  'https://res.cloudinary.com/stratmachine/image/upload/v1617737720/objects/mildlee-8N6z4yXUkwY-unsplash_cqs8nk.jpg'
+  }
 ]
-async function identify(uri) {
-    
-    const url = uri       
+async function identify(item) {
+    const description = item.description
+    const url = item.url    
     const options = {
       maxCandidates: 5,
       language: "en"
     };
+
     client
       .describeImage(url, options)
       .then((result) => {
-        console.log("The result is:");
+        console.log("---------------------------------")
+        console.log(`This is a ${description}`)
+        console.log("The Vision Machine saw:");
         console.log(result);
       })
       .catch((err) => {
@@ -49,7 +97,7 @@ async function identify(uri) {
 
 async function main() {
     
-    items.forEach(i => identify(i))    
+    items.forEach(i => identify(i)) 
   }
   
   main();
