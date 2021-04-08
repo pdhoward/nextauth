0b2ae27bb6fce08da9715799d6769a76976a3572
pdhoward <patrick.howard@hotmail.com>
2021-04-08 09:10:12 -0500

    expriment with 2nd test camera

---
+++ client/pages/capture.js
@@ -0,0 +1,44 @@
+import React, { Fragment, useState } from "react";
+import ReactDOM from "react-dom";
+import { Camera } from "../components/camera";
+import { Root, Preview, Footer, GlobalStyle } from "../styles/capture";
+
+const Capture = () => {
+  const [isCameraOpen, setIsCameraOpen] = useState(false);
+  const [cardImage, setCardImage] = useState();
+
+  return (
+    <Fragment>
+      <Root>
+        {isCameraOpen && (
+          <Camera
+            onCapture={blob => setCardImage(blob)}
+            onClear={() => setCardImage(undefined)}
+          />
+        )}
+
+        {cardImage && (
+          <div>
+            <h2>Preview</h2>
+            <Preview src={cardImage && URL.createObjectURL(cardImage)} />
+          </div>
+        )}
+
+        <Footer>
+          <button onClick={() => setIsCameraOpen(true)}>Open Camera</button>
+          <button
+            onClick={() => {
+              setIsCameraOpen(false);
+              setCardImage(undefined);
+            }}
+          >
+            Close Camera
+          </button>
+        </Footer>
+      </Root>
+      <GlobalStyle />
+    </Fragment>
+  );
+}
+
+export default Capture;
