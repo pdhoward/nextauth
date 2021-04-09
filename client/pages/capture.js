15c2ec37f3d8a4b158e235d6144567fbcc339899
pdhoward <patrick.howard@hotmail.com>
2021-04-09 15:49:40 -0500

    experiment with uppy

--- client/pages/capture.js
+++ client/pages/capture.js
@@ -1,44 +1,51 @@
-import React from 'react'
+import React, { Component, useState } from 'react';
+import {Button} from 'react-bootstrap'
 import Uppy from '@uppy/core'
 import Tus from '@uppy/tus'
-import { DragDrop } from '@uppy/react'
+import Webcam from '@uppy/webcam'
+import XHRUpload from '@uppy/xhr-upload'
+//import Dashboard from '@uppy/react/lib/Dashboard'
+import {DashboardModal, useUppy} from '@uppy/react'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '@uppy/core/dist/style.css'
-import '@uppy/drag-drop/dist/style.css'
+import '@uppy/dashboard/dist/style.css'
 
-const uppy = new Uppy({
-  meta: { type: 'avatar' },
-  restrictions: { maxNumberOfFiles: 1 },
-  autoProceed: true
-})
 
-uppy.use(Tus, { endpoint: '/upload' })
+//https://medium.com/@BrodaNoel/conditional-rendering-in-react-and-jsx-the-solution-7c80beba1e36
 
-uppy.on('complete', (result) => {
-  const url = result.successful[0].uploadURL
-  store.dispatch({
-    type: 'SET_USER_AVATAR_URL',
-    payload: { url: url }
-  })
-})
+function MyComponent () {
+  // const uppy = useUppy(() => {
+  //   return new Uppy()
+  //     .use(Tus, { endpoint: 'https://tusd.tusdemo.net/files' })
+  //     .use(Webcam)
+  // })
+  const [show, setShow] = useState(false)
+  const [uppy, setUppy] = useState(null)
 
-const Capture = ({ currentAvatar }) => {
+  const initiate = () => {
+    return new Promise ((resolve, reject) => {
+      console.log('Trigger intitiate')
+      let uppyObj = new Uppy()
+          .use(Tus, { endpoint: 'https://tusd.tusdemo.net/files' })
+          .use(Webcam)
+      resolve(uppyObj)
+    })
+  }
+  const renderDash = async () => {
+    console.log('Click')
+    setUppy(await initiate())
+    console.log('Back to render')
+    show ? setShow(false) : setShow(true)  
+    console.log(show)  
+  }
+
   return (
-    <div>
-      <img src={currentAvatar} alt="Current Avatar" />
-      <DragDrop
-        uppy={uppy}
-        locale={{
-          strings: {
-            // Text to show on the droppable area.
-            // `%{browse}` is replaced with a link that opens the system file selection dialog.
-            dropHereOr: 'Drop here or %{browse}',
-            // Used as the label for the link that opens the system file selection dialog.
-            browse: 'browse'
-          }
-        }}
-      />
-    </div>
+  <>
+    <Button variant="primary">Primary</Button>{' '}
+    <Button onClick={() => renderDash()} variant="primary">Click</Button>
+    {show ? (<DashboardModal uppy={uppy} />) : (null)}
+  </>
   )
 }
 
-export default Capture;
+export default MyComponent
--- client/pages/index.js
+++ client/pages/index.js
@@ -10,7 +10,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />       
       </Head>
 
-      <Capture /> 
+      <Capture />
+      
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
@@ -21,6 +22,7 @@ export default function Home() {
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>    
+     
     </>
   );
 }
