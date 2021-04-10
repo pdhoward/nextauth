b62aae7963c49edabadfbec87dcfca97faf60fd4
pdhoward <patrick.howard@hotmail.com>
2021-04-10 14:48:46 -0500

    final camera test - keep as auth demo

--- client/pages/index.js
+++ client/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />       
       </Head>
 
-      <Capture />
+      
       
       <footer className={styles.footer}>
         <a
